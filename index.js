b91a458451cd5445af001ba6e256982570ca29a8
PJ Joshi <joshipv2@gmail.com>
2019-03-25 19:20:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if string.toLowerCase() === srting {
+  if (string.toLowerCase() === srting) {
     return "I can\'t hear you!"
-  } else if string.toUpperCase() === string {
+  } else if (string.toUpperCase() === string) {
     return "YES INDEED!"
-  } else if string.mixedCase() === "I love you, Grandma." {
+  } else if (string.mixedCase() === "I love you, Grandma.") {
     return "I love you too."
   }
 }
